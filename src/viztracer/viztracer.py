45dc486a260e48174629dc01c6cbeb9519920c1d
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-08-14 22:46:22 -0700

    Add output file chain for constructor

--- src/viztracer/viztracer.py
+++ src/viztracer/viztracer.py
@@ -7,7 +7,13 @@ from .tracer import _VizTracer
 # This is the interface of the package. Almost all user should use this
 # class for the functions
 class VizTracer(_VizTracer):
-    def __init__(self, tracer="c", verbose=1, max_stack_depth=-1, include_files=None, exclude_files=None):
+    def __init__(self, 
+            tracer="c", 
+            verbose=1, 
+            max_stack_depth=-1, 
+            include_files=None, 
+            exclude_files=None,
+            output_file="result.html"):
         super().__init__(
                 tracer=tracer,
                 max_stack_depth=max_stack_depth,
@@ -15,16 +21,19 @@ class VizTracer(_VizTracer):
                 exclude_files=exclude_files
         )
         self.verbose = verbose
+        self.output_file=output_file
 
-    def run(self, command, output_file="./result.html"):
+    def run(self, command, output_file=None):
         self.start()
         exec(command)
         self.stop()
-        self.save()
+        self.save(output_file)
 
-    def save(self, output_file="./result.html"):
+    def save(self, output_file=None):
         if not self.parsed:
             self.parse()
+        if output_file is None:
+            output_file = self.output_file
         file_type = output_file.split(".")[-1]
         if file_type == "html":
             with open(output_file, "w") as f:
