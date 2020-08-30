db4a6e5d5f3a1eee98dd171b96b4623110653bfe
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-08-29 18:45:04 -0700

    Make snaptrace module oop (#6)
    Make snaptrace module oop
    
    If not using fork, we parse the data first to avoid pickle tracer, which is a c structure, not python stuff
    
    Use abspath when spawn a new process.

--- src/viztracer/modules/snaptrace.h
+++ src/viztracer/modules/snaptrace.h
@@ -15,4 +15,87 @@
 
 #define CHECK_FLAG(reg, flag) (((reg) & (flag)) != 0) 
 
+typedef enum _NodeType {
+    EVENT_NODE = 0,
+    FEE_NODE = 1,
+    INSTANT_NODE = 2,
+    COUNTER_NODE = 3,
+    OBJECT_NODE = 4
+} NodeType;
+
+struct FEEData {
+    PyObject* file_name;
+    PyObject* args;
+    int first_lineno;
+    PyObject* func_name;
+    int type;
+    double dur;
+    struct EventNode* parent;
+};
+
+struct InstantData {
+    PyObject* name;
+    PyObject* args;
+    PyObject* scope;
+};
+
+struct CounterData {
+    PyObject* name;
+    PyObject* args;
+};
+
+struct ObjectData {
+    PyObject* name;
+    PyObject* args;
+    PyObject* id;
+    PyObject* ph;
+};
+
+struct EventNode {
+    NodeType ntype;
+    struct EventNode* next;
+    struct EventNode* prev;
+    double ts;
+    unsigned long tid;
+    union {
+        struct FEEData fee;
+        struct InstantData instant;
+        struct CounterData counter;
+        struct ObjectData object;
+    } data;
+};
+
+
+struct ThreadInfo {
+    int paused;
+    int curr_stack_depth;
+    int ignore_stack_depth;
+    unsigned long tid;
+    struct EventNode* stack_top;
+};
+
+typedef struct {
+    PyObject_HEAD
+#if _WIN32
+    DWORD dwTlsIndex;
+#else
+    pthread_key_t thread_key;
+#endif
+    int collecting;
+    // When we do fork_save(), we want to keep the pid. This is a 
+    // mechanism for child process to keep the parent's pid. If 
+    // this value is 0, then the program gets pid before parsing,
+    // otherwise it uses this pid
+    long fix_pid;
+    unsigned long total_entries;
+    unsigned int check_flags;
+    int verbose;
+    char* lib_file_path;
+    int max_stack_depth;
+    PyObject* include_files;
+    PyObject* exclude_files;
+    struct EventNode* buffer_head;
+    struct EventNode* buffer_tail;
+} TracerObject;
+
 #endif
