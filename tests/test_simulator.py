771b860784461eb108f0a2879d5d6b062bfd6ffc
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-09-09 15:43:23 -0700

    print result!!!

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -142,7 +142,7 @@ class TestSimulator(unittest.TestCase):
         for _ in range(100):
             sim.command("n")
         result = sim.command("w")
-        print(result)
+        print("result: {}".format(result))
         print(self.get_func_stack(result))
         self.assertEqual(self.get_func_stack(result), ["join"])
         for _ in range(101):
