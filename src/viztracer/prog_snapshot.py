4cbeda8ebb9f7203a1529d68facd99bc575a9160
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-09-08 23:42:26 -0700

    Add license banners

--- src/viztracer/prog_snapshot.py
+++ src/viztracer/prog_snapshot.py
@@ -1,3 +1,6 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
 try:
     import orjson as json
 except ImportError:
