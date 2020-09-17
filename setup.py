4893f34db28f27fd339388c2262c6c4ec211d68f
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-09-16 17:19:07 -0700

    It's time to start supporting 3.9

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
