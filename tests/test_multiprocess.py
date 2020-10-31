88f68b7353a7b35c870bcb4876d4d90d5d662c4a
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-10-31 15:44:05 -0700

    Add multiprocess feature

--- tests/test_multiprocess.py
+++ tests/test_multiprocess.py
@@ -3,6 +3,7 @@
 
 
 import os
+import sys
 from .cmdline_tmpl import CmdlineTmpl
 
 
@@ -22,6 +23,42 @@ def fib(n):
 fib(5)
 """
 
+file_fork = \
+"""
+import os
+import time
+
+pid = os.fork()
+
+if pid > 0:
+    time.sleep(0.1)
+    print("parent")
+else:
+    print("child")
+"""
+
+file_multiprocessing = \
+"""
+import multiprocessing
+from multiprocessing import Process
+import time
+
+
+def fib(n):
+    if n < 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+def f():
+    fib(5)
+
+if __name__ == "__main__":
+    fib(2)
+    p = Process(target=f)
+    p.start()
+    p.join()
+    time.sleep(0.1)
+"""
 
 class TestSubprocess(CmdlineTmpl):
     def setUp(self):
@@ -38,3 +75,24 @@ class TestSubprocess(CmdlineTmpl):
                 pids.add(entry["pid"])
             self.assertGreater(len(pids), 1)
         self.template(["viztracer", "--log_subprocess", "-o", "result.json", "cmdline_test.py"], expected_output_file="result.json", script=file_parent, check_func=check_func)
+
+
+class TestMultiprocessing(CmdlineTmpl):
+    def test_os_fork(self):
+        def check_func(data):
+            pids = set()
+            for entry in data["traceEvents"]:
+                pids.add(entry["pid"])
+            self.assertGreater(len(pids), 1)
+        if sys.platform in ["linux", "linux2"]:
+            self.template(["viztracer", "--log_multiprocess", "-o", "result.json", "cmdline_test.py"], expected_output_file="result.json", script=file_fork, check_func=check_func)
+
+    def test_multiprosessing(self):
+        def check_func(data):
+            pids = set()
+            for entry in data["traceEvents"]:
+                pids.add(entry["pid"])
+            self.assertGreater(len(pids), 1)
+        
+        if sys.platform in ["linux", "linux2"]:
+            self.template(["viztracer", "--log_multiprocess", "-o", "result.json", "cmdline_test.py"], expected_output_file="result.json", script=file_multiprocessing, check_func=check_func)
