3bea8ee14e7cbef59d321274fdb4e80d4b95a70e
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-11-06 19:29:36 -0800

    Add a multithread test with smaller buffer

--- tests/test_multithread.py
+++ tests/test_multithread.py
@@ -43,3 +43,26 @@ class TestMultithread(BaseTmpl):
         tracer.stop()
         entries = tracer.parse()
         self.assertGreater(entries, 160)
+
+    def test_with_small_buffer(self):
+        tracer = VizTracer(tracer_entries=300)
+        tracer.start()
+
+        thread1 = MyThread()
+        thread2 = MyThread()
+        thread3 = MyThread()
+        thread4 = MyThread()
+
+        thread1.start()
+        thread2.start()
+        thread3.start()
+        thread4.start()
+
+        threads = [thread1, thread2, thread3, thread4]
+
+        for thread in threads:
+            thread.join()
+
+        tracer.stop()
+        entries = tracer.parse()
+        self.assertEqual(entries, 300)
