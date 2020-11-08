5d6c9e79dd6d8ef8750613fe74194a3e377e9d9b
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-11-07 18:48:51 -0800

    Add cwd to sys.path if running modules

--- src/viztracer/main.py
+++ src/viztracer/main.py
@@ -254,6 +254,7 @@ class VizUI:
             "modname": self.options.module
         }
         sys.argv = [self.options.module] + self.command[:]
+        sys.path.append(os.getcwd())
         return self.run_code(code, global_dict)
 
     def run_command(self):
