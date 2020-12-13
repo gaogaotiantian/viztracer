0b0974e1ab04b560d7530fa43ccacfc469dbc17b
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-12-12 16:48:27 -0800

    Update examples

--- example/src/function_args_return.py
+++ example/src/function_args_return.py
@@ -8,8 +8,8 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 
-with VizTracer(log_function_args=True, 
-               log_return_value=True, 
+with VizTracer(log_func_args=True, 
+               log_func_retval=True, 
                file_info=True,
                output_file=os.path.join(os.path.dirname(__file__), "../", "json/function_args_return.json")):
     fib(6)
