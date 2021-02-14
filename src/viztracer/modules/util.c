8a97fe0c63fa9245588a71c3b77dba8bebe4a5ae
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-02-13 18:08:31 -0800

    put windows.h in #

--- src/viztracer/modules/util.c
+++ src/viztracer/modules/util.c
@@ -1,7 +1,9 @@
 // Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 // For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
 
+#if _WIN32
 #include <windows.h>
+#endif
 #include <Python.h>
 #include <time.h>
 
