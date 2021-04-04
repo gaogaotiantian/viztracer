bd830283cc7d6d33c8d5c41dfd8886e587de476f
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-04-04 16:13:38 -0700

    Fix lint

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -194,12 +194,16 @@ class TestDecorator(BaseTmpl):
             time.sleep(0.5)
 
             def t1():
-                a = subprocess.run(["ls result_my_function2*.json"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                a = subprocess.run(
+                    ["ls result_my_function2*.json"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
                 self.assertEqual(a.returncode, 0)
             self.assertTrueTimeout(t1, 10)
 
             def t2():
-                a = subprocess.run(["rm result_my_function2*.json"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                a = subprocess.run(
+                    ["rm result_my_function2*.json"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
                 self.assertEqual(a.returncode, 0)
             self.assertTrueTimeout(t2, 10)
 
