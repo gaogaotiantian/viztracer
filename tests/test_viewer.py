b18c5e4f04fd4b0ba9c8c6491506174b73600243
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-15 13:47:17 -0700

    Skip the test that requires Ctrl+C on Windows

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -7,6 +7,7 @@ import os
 import signal
 import socket
 import subprocess
+import sys
 import time
 import tempfile
 import unittest.mock
@@ -33,7 +34,7 @@ class Viewer(unittest.TestCase):
     def stop(self):
         self.process.send_signal(signal.SIGINT)
         self.process.wait()
-        self.assertTrue(self.process.returncode == 0)
+        self.assertEqual(self.process.returncode, 0)
 
     def _wait_until_socket_on(self):
         for _ in range(10):
@@ -48,51 +49,50 @@ class Viewer(unittest.TestCase):
 
 
 class TestViewer(CmdlineTmpl):
+    @unittest.skipIf(sys.platform == "win32", "Can't send Ctrl+C reliably on Windows")
     def test_json(self):
         json_script = '{"traceEvents":[{"ph":"M","pid":17088,"tid":17088,"name":"process_name","args":{"name":"MainProcess"}},{"ph":"M","pid":17088,"tid":17088,"name":"thread_name","args":{"name":"MainThread"}},{"pid":17088,"tid":17088,"ts":20889378694.06,"dur":1001119.1,"name":"time.sleep","caller_lineno":4,"ph":"X","cat":"FEE"},{"pid":17088,"tid":17088,"ts":20889378692.96,"dur":1001122.5,"name":"<module> (/home/gaogaotiantian/programs/codesnap/scrabble4.py:1)","caller_lineno":238,"ph":"X","cat":"FEE"},{"pid":17088,"tid":17088,"ts":20889378692.46,"dur":1001124.7,"name":"builtins.exec","caller_lineno":238,"ph":"X","cat":"FEE"}],"viztracer_metadata":{"version":"0.12.0"}}'  # noqa: E501
         try:
-            _, path = tempfile.mkstemp(suffix=".json", text=True)
-            with open(path, "w") as f:
+            with tempfile.NamedTemporaryFile(mode="w", suffix=".json", delete=False) as f:
                 f.write(json_script)
-            v = Viewer(path)
-            v.run()
-            time.sleep(0.5)
-            resp = urllib.request.urlopen("http://127.0.0.1:9001")
-            self.assertTrue(resp.code == 200)
-            v.stop()
+                v = Viewer(f.name)
+                v.run()
+                time.sleep(0.5)
+                resp = urllib.request.urlopen("http://127.0.0.1:9001")
+                v.stop()
+                self.assertTrue(resp.code == 200)
         finally:
-            os.remove(path)
+            os.remove(f.name)
 
+    @unittest.skipIf(sys.platform == "win32", "Can't send Ctrl+C reliably on Windows")
     def test_html(self):
         html = '<html></html>'
         try:
-            _, path = tempfile.mkstemp(suffix=".html", text=True)
-            with open(path, "w") as f:
+            with tempfile.NamedTemporaryFile(mode="w", suffix=".html", delete=False) as f:
                 f.write(html)
-            v = Viewer(path)
-            v.run()
-            time.sleep(0.5)
-            resp = urllib.request.urlopen("http://127.0.0.1:9001")
-            self.assertTrue(resp.code == 200)
-            v.stop()
+                v = Viewer(f.name)
+                v.run()
+                time.sleep(0.5)
+                resp = urllib.request.urlopen("http://127.0.0.1:9001")
+                v.stop()
+                self.assertTrue(resp.code == 200)
         finally:
-            os.remove(path)
+            os.remove(f.name)
 
     def test_once(self):
         html = '<html></html>'
         try:
-            _, path = tempfile.mkstemp(suffix=".html", text=True)
-            with open(path, "w") as f:
+            with tempfile.NamedTemporaryFile(mode="w", suffix=".html", delete=False) as f:
                 f.write(html)
-            v = Viewer(path, once=True)
-            v.run()
-            time.sleep(0.5)
-            resp = urllib.request.urlopen("http://127.0.0.1:9001")
-            self.assertTrue(resp.code == 200)
-            v.process.wait()
-            self.assertTrue(v.process.returncode == 0)
+                v = Viewer(f.name, once=True)
+                v.run()
+                time.sleep(0.5)
+                resp = urllib.request.urlopen("http://127.0.0.1:9001")
+                v.process.wait()
+                self.assertTrue(resp.code == 200)
+                self.assertTrue(v.process.returncode == 0)
         finally:
-            os.remove(path)
+            os.remove(f.name)
 
     def test_invalid(self):
         self.template(["vizviewer", "do_not_exist.json"], success=False, expected_output_file=None)
