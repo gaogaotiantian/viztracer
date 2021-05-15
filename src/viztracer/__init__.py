c6206d238372a78d7ca9d27b603d428099ad1bda
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-15 16:28:15 -0700

    Ready for 0.13.0

--- src/viztracer/__init__.py
+++ src/viztracer/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
 
-__version__ = "0.12.3"
+__version__ = "0.13.0"
 
 
 from .viztracer import VizTracer
