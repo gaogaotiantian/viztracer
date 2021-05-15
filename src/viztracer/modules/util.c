b17d979bdd8da43a8c186b0319e6be12ef39bad0
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-15 16:13:07 -0700

    Use inline for startwith and fix ts

--- src/viztracer/modules/util.c
+++ src/viztracer/modules/util.c
@@ -21,34 +21,6 @@ void Print_Py(PyObject* o)
     Py_DECREF(repr);
 }
 
-// target and prefix has to be NULL-terminated
-int startswith(const char* target, const char* prefix)
-{
-    while(*target != 0 && *prefix != 0) {
-#if _WIN32
-        // Windows path has double slashes and case-insensitive
-        if (*prefix == '\\' && prefix[-1] == '\\') {
-            prefix++;
-        }
-        if (*target == '\\' && target[-1] == '\\') {
-            target++;
-        }
-        if (*target != *prefix && *target != *prefix - ('a'-'A') && *target != *prefix + ('a'-'A')) {
-            return 0;
-        }
-#else
-        if (*target != *prefix) {
-            return 0;
-        }
-#endif
-        target++;
-        prefix++;
-    }
-
-    return (*prefix) == 0;
-}
-
-
 double get_ts(void)
 {
     static double prev_ts = 0;
--- src/viztracer/modules/util.h
+++ src/viztracer/modules/util.h
@@ -6,7 +6,33 @@
 #define __SNAPTRACE_UTIL_H__
 
 void Print_Py(PyObject* o);
-int startswith(const char* target, const char* prefix);
 double get_ts(void);
+
+// target and prefix has to be NULL-terminated
+inline int startswith(const char* target, const char* prefix)
+{
+    while(*target != 0 && *prefix != 0) {
+#if _WIN32
+        // Windows path has double slashes and case-insensitive
+        if (*prefix == '\\' && prefix[-1] == '\\') {
+            prefix++;
+        }
+        if (*target == '\\' && target[-1] == '\\') {
+            target++;
+        }
+        if (*target != *prefix && *target != *prefix - ('a'-'A') && *target != *prefix + ('a'-'A')) {
+            return 0;
+        }
+#else
+        if (*target != *prefix) {
+            return 0;
+        }
+#endif
+        target++;
+        prefix++;
+    }
+
+    return (*prefix) == 0;
+}
 
 #endif
