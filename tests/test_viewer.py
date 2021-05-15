4e95be7e90b9abd06a2402ef81d6d185caa68e0c
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-14 20:26:50 -0700

    Use tempfile

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -8,6 +8,7 @@ import signal
 import subprocess
 import sys
 import time
+import tempfile
 import unittest.mock
 import urllib.request
 
@@ -37,38 +38,41 @@ class TestViewer(CmdlineTmpl):
     def test_json(self):
         json_script = '{"traceEvents":[{"ph":"M","pid":17088,"tid":17088,"name":"process_name","args":{"name":"MainProcess"}},{"ph":"M","pid":17088,"tid":17088,"name":"thread_name","args":{"name":"MainThread"}},{"pid":17088,"tid":17088,"ts":20889378694.06,"dur":1001119.1,"name":"time.sleep","caller_lineno":4,"ph":"X","cat":"FEE"},{"pid":17088,"tid":17088,"ts":20889378692.96,"dur":1001122.5,"name":"<module> (/home/gaogaotiantian/programs/codesnap/scrabble4.py:1)","caller_lineno":238,"ph":"X","cat":"FEE"},{"pid":17088,"tid":17088,"ts":20889378692.46,"dur":1001124.7,"name":"builtins.exec","caller_lineno":238,"ph":"X","cat":"FEE"}],"viztracer_metadata":{"version":"0.12.0"}}'  # noqa: E501
         try:
-            with open("test.json", "w") as f:
+            _, path = tempfile.mkstemp(suffix=".json", text=True)
+            with open(path, "w") as f:
                 f.write(json_script)
-            v = Viewer("test.json")
+            v = Viewer(path)
             v.run()
             time.sleep(0.5)
             resp = urllib.request.urlopen("http://127.0.0.1:9001")
             self.assertTrue(resp.code == 200)
             v.stop()
         finally:
-            os.remove("test.json")
+            os.remove(path)
 
     @unittest.skipIf(sys.platform == "darwin", "MacOS has a high security check for multiprocessing")
     def test_html(self):
         html = '<html></html>'
         try:
-            with open("test.html", "w") as f:
+            _, path = tempfile.mkstemp(suffix=".html", text=True)
+            with open(path, "w") as f:
                 f.write(html)
-            v = Viewer("test.html")
+            v = Viewer(path)
             v.run()
             time.sleep(0.5)
             resp = urllib.request.urlopen("http://127.0.0.1:9001")
             self.assertTrue(resp.code == 200)
             v.stop()
         finally:
-            os.remove("test.html")
+            os.remove(path)
 
     def test_once(self):
         html = '<html></html>'
         try:
-            with open("test.html", "w") as f:
+            _, path = tempfile.mkstemp(suffix=".html", text=True)
+            with open(path, "w") as f:
                 f.write(html)
-            v = Viewer("test.html", once=True)
+            v = Viewer(path, once=True)
             v.run()
             time.sleep(0.5)
             resp = urllib.request.urlopen("http://127.0.0.1:9001")
@@ -76,7 +80,7 @@ class TestViewer(CmdlineTmpl):
             v.process.wait()
             self.assertTrue(v.process.returncode == 0)
         finally:
-            os.remove("test.html")
+            os.remove(path)
 
     def test_invalid(self):
         self.template(["vizviewer", "do_not_exist.json"], success=False, expected_output_file=None)
