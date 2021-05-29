3ca583e917402b51e68fd411297d0732690cc146
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-29 13:37:57 -0700

    Update coverage test

--- tests/test_flamegraph.py
+++ tests/test_flamegraph.py
@@ -48,3 +48,10 @@ class TestFlameGraph(BaseTmpl):
         for callsite_info in data:
             self.assertIn("name", callsite_info)
             self.assertIn("flamegraph", callsite_info)
+
+        # Test if FlameGraph can handle empty tree
+        sample_data["traceEvents"].append({"ph":"M","pid":1,"tid":1,"name":"thread_name","args":{"name":"MainThread"}})
+        fg = FlameGraph(sample_data)
+        self.assertEqual(len(fg.trees), 6)
+        data = fg.dump_to_perfetto()
+        self.assertEqual(len(data), 5)
