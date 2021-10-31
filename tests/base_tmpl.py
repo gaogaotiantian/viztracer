488fe5ea3b1e0c8ed9ae847dea45882259593444
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-10-30 23:47:45 -0700

    Fix lint

--- tests/base_tmpl.py
+++ tests/base_tmpl.py
@@ -18,7 +18,8 @@ class BaseTmpl(TestCase):
     def assertEventNumber(self, data, expected_entries):
         entries = [1 for entry in data["traceEvents"] if entry["ph"] != "M"]
         entries_count = len(entries)
-        self.assertEqual(entries_count, expected_entries, f"Event number incorrect, {entries_count}(expected {expected_entries}) - {entries}")
+        self.assertEqual(entries_count, expected_entries,
+                         f"Event number incorrect, {entries_count}(expected {expected_entries}) - {entries}")
 
     def assertFileExists(self, path, timeout=None):
         if timeout is None:
