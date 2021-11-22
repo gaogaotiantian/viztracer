125766fbc3c08a1bdbf216fd3e1eea68d0ba01da
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-11-21 22:12:53 -0800

    Skip windows for SIGTERM

--- tests/test_multiprocess.py
+++ tests/test_multiprocess.py
@@ -180,6 +180,7 @@ class TestSubprocess(CmdlineTmpl):
                           expected_output_file=None)
             self.assertEqual(len(os.listdir(tmpdir)), 1)
 
+    @unittest.skipIf(sys.platform == "win32", "Can't get anything on Windows with SIGTERM")
     def test_term(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             self.template(["viztracer", "-o", os.path.join(tmpdir, "result.json"), "--subprocess_child", "cmdline_test.py"],
