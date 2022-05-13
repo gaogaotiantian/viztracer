571c28fb74762c4777bbf87a4449e771576fff7e
gaogaotiantian <gaogaotiantian@hotmail.com>
2022-05-12 20:19:36 -0700

    Ready for 0.15.2

--- src/viztracer/__init__.py
+++ src/viztracer/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
 
-__version__ = "0.15.1"
+__version__ = "0.15.2"
 
 
 from .cellmagic import load_ipython_extension
