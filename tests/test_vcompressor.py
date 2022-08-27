4142bb6ef8833bef4d90096019e81c3ef756f682
gaogaotiantian <gaogaotiantian@hotmail.com>
2022-08-26 21:59:14 -0700

    Add vcompressor test

---
+++ tests/test_vcompressor.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+
+import os
+import tempfile
+
+from .cmdline_tmpl import CmdlineTmpl
+from .util import get_json_file_path
+
+
+class TestVCompressor(CmdlineTmpl):
+    def test_basic(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            cvf_path = os.path.join(tmpdir, "result.cvf")
+            dup_json_path = os.path.join(tmpdir, "result.json")
+            self.template(
+                ["viztracer", "-o", cvf_path, "--compress", get_json_file_path("vdb_multithread.json")],
+                expected_output_file=cvf_path, cleanup=False
+            )
+
+            self.template(
+                ["viztracer", "-o", dup_json_path, "--decompress", cvf_path],
+                expected_output_file=dup_json_path
+            )
