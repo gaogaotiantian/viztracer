9aecb7b19d51055439d276e2e1b5d596593599e2
Tianshun_Peng <36334980+TTianshun@users.noreply.github.com>
2023-03-28 01:53:04 +0800

    Fix test failure for jupyter (#319)
    * Used ipywidgets 8.0.4 because 8.0.5 has some bugs
    * Updated the magic cell test to always execute the test

--- tests/test_jupyter.py
+++ tests/test_jupyter.py
@@ -10,14 +10,13 @@ class TestJupyter(BaseTmpl):
     def setUp(self):
         super().setUp()
         try:
-            from IPython.testing.globalipapp import start_ipython
-            self.start_ipython = start_ipython
+            from IPython.terminal.interactiveshell import TerminalInteractiveShell
+            self.ip = TerminalInteractiveShell.instance()
         except ImportError:
             self.skipTest("No Jupyter, skip Jupyter test")
 
     def test_cellmagic(self):
-        ip = self.start_ipython()
-        ip.magic("load_ext viztracer")
-        ip.run_cell_magic(magic_name="viztracer", line="", cell="print(1)")
+        self.ip.run_line_magic("load_ext", "viztracer")
+        self.ip.run_cell_magic(magic_name="viztracer", line="", cell="print(1)")
         self.assertFileExists("./viztracer_report.json")
         os.remove("./viztracer_report.json")
