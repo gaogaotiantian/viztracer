197e19df4918fc0189e96e5016579281ca09c927
Mark Mayo <mark@there.co.nz>
2023-06-09 13:36:25 +1200

    fix deprecated warn call

--- example/src/logging_integration.py
+++ example/src/logging_integration.py
@@ -5,7 +5,7 @@ from viztracer import VizLoggingHandler, get_tracer
 
 def fib(n):
     if n < 2:
-        logging.warn("Base case, return 1")
+        logging.warning("Base case, return 1")
         return 1
     logging.info(f"Recursive, working on {n}")
     return fib(n - 1) + fib(n - 2)
