526e70002ad00d46df7963201039bf30c6850721
alexeykozhevin <alexey.a.kozhevin@gmail.com>
2023-08-08 15:28:17 +0000

    Add dynamic_tracer_check parameter

--- src/viztracer/decorator.py
+++ src/viztracer/decorator.py
@@ -56,39 +56,52 @@ def trace_and_save(method: Optional[Callable] = None, output_dir: str = "./", **
         return inner(method)
     return inner
 
+def _log_sparse_wrapper(func: Optional[Callable] = None, stack_depth: int = 0,
+                        dynamic_tracer_check: bool = False) -> Callable:
+    if not dynamic_tracer_check:
+        tracer = get_tracer()
+        if tracer is None or not tracer.log_sparse:
+            if func is None:
+                return lambda f: f
+            return func
 
-def log_sparse(func: Optional[Callable] = None, stack_depth: int = 0) -> Callable:
-    if func is None:
-        def inner(dec_func: Callable) -> Callable:
-            @functools.wraps(dec_func)
-            def wrapper(*args, **kwargs) -> Any:
-                tracer = get_tracer()
-                if tracer is None or not tracer.log_sparse:
-                    return dec_func(*args, **kwargs)
-                assert isinstance(tracer, VizTracer)
-                if not tracer.enable:
-                    orig_max_stack_depth = tracer.max_stack_depth
-                    tracer.max_stack_depth = stack_depth
-                    tracer.start()
-                    ret = dec_func(*args, **kwargs)
-                    tracer.stop()
-                    tracer.max_stack_depth = orig_max_stack_depth
-                    return ret
-                else:
-                    return dec_func(*args, **kwargs)
-            return wrapper
-        return inner
+    if stack_depth > 0:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            local_tracer = get_tracer() if dynamic_tracer_check else tracer
+
+            if local_tracer is None or not local_tracer.log_sparse:
+                if func is None:
+                    return lambda f: f
+                return func
+
+            assert isinstance(local_tracer, VizTracer)
+            if not local_tracer.enable:
+                orig_max_stack_depth = local_tracer.max_stack_depth
+                local_tracer.max_stack_depth = stack_depth
+                local_tracer.start()
+                ret = func(*args, **kwargs)
+                local_tracer.stop()
+                local_tracer.max_stack_depth = orig_max_stack_depth
+                return ret
+            else:
+                return func(*args, **kwargs)
+        return wrapper
     else:
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> Any:
-            tracer = get_tracer()
-            if tracer is None or not tracer.log_sparse:
-                return func(*args, **kwargs)
+            local_tracer = get_tracer() if dynamic_tracer_check else tracer
+
+            if local_tracer is None or not local_tracer.log_sparse:
+                if func is None:
+                    return lambda f: f
+                return func
+
             assert callable(func)
-            assert isinstance(tracer, VizTracer)
-            start = tracer.getts()
+            assert isinstance(local_tracer, VizTracer)
+            start = local_tracer.getts()
             ret = func(*args, **kwargs)
-            dur = tracer.getts() - start
+            dur = local_tracer.getts() - start
             code = func.__code__
             raw_data = {
                 "ph": "X",
@@ -97,7 +110,16 @@ def log_sparse(func: Optional[Callable] = None, stack_depth: int = 0) -> Callabl
                 "dur": dur,
                 "cat": "FEE",
             }
-            tracer.add_raw(raw_data)
+            local_tracer.add_raw(raw_data)
             return ret
 
         return wrapper
+
+
+def _log_sparse_wrapper_with_args(stack_depth: int = 0, dynamic_tracer_check: bool = False) -> Callable:
+    return functools.partial(_log_sparse_wrapper, stack_depth=stack_depth, dynamic_tracer_check=dynamic_tracer_check)
+
+def log_sparse(func: Optional[Callable] = None, stack_depth: int = 0, dynamic_tracer_check: bool = False) -> Callable:
+    if func is not None:
+        return _log_sparse_wrapper(func=func, stack_depth=stack_depth, dynamic_tracer_check=dynamic_tracer_check)
+    return _log_sparse_wrapper_with_args(stack_depth=stack_depth, dynamic_tracer_check=dynamic_tracer_check)
