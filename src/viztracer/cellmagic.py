7fb9dcb104efc4ce896fe88810ccf6df6534e74f
alexeykozhevin <alexey.a.kozhevin@gmail.com>
2023-08-09 10:48:17 +0000

    Add kwargs to %%viztracer

--- src/viztracer/cellmagic.py
+++ src/viztracer/cellmagic.py
@@ -1,5 +1,7 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+import re
+import inspect
 
 try:
     from IPython.core.magic import (Magics, cell_magic,  # type: ignore
@@ -15,13 +17,15 @@ try:
 
             from .viewer import ServerThread
             from .viztracer import VizTracer
+            tracer_kwargs, viewer_kwargs = self.make_kwargs(
+                line, inspect.signature(VizTracer), inspect.signature(ServerThread)
+            )
             code = self.shell.transform_cell(cell)
-            file_path = "./viztracer_report.json"
-            with VizTracer(verbose=0, output_file=file_path):
+            with VizTracer(**tracer_kwargs):
                 exec(code, local_ns, local_ns)
 
             def view():  # pragma: no cover
-                server = ServerThread(file_path, once=True)
+                server = ServerThread(**viewer_kwargs)
                 server.start()
                 server.ready.wait()
                 import webbrowser
@@ -31,6 +35,36 @@ try:
             button.on_click(lambda b: view())
 
             display(button)
+
+        def make_kwargs(self, line, tracer_signature, viewer_signature):
+            line = line.split('#')[0]
+            pattern = r'(\w+=\w+)\s'
+            default_path = "./viztracer_report.json"
+
+            tracer_kwargs = {'verbose': 0, 'output_file': default_path}
+            viewer_kwargs = {'path': default_path, 'once': True}
+
+            for item in re.split(pattern, line):
+                if item == '':
+                    continue
+                name, value = item.split('=')
+                if name == 'output_file':
+                    tracer_kwargs['output_file'] = value
+                    viewer_kwargs['path'] = value
+                elif name in viewer_signature.parameters:
+                    annotation = viewer_signature.parameters[name].annotation
+                    kwargs = viewer_kwargs
+                elif name in tracer_signature.parameters:
+                    annotation = tracer_signature.parameters[name].annotation
+                    kwargs = tracer_kwargs
+                else:
+                    raise ValueError(f'{name} is not a parameter of VizTracer or ServerThread')
+
+                if value == 'None':
+                    kwargs[name] = None
+                else:
+                    kwargs[name] = annotation(value)
+            return tracer_kwargs, viewer_kwargs
 
 except ImportError:  # pragma: no cover
     pass
