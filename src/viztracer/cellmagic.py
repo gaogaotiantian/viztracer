3f51ae853761d62d259f79f97bfa2f1ba81bf7c7
alexeykozhevin <alexey.a.kozhevin@gmail.com>
2023-10-23 16:30:13 +0000

    Remove verbose parameter of magic

--- src/viztracer/cellmagic.py
+++ src/viztracer/cellmagic.py
@@ -15,8 +15,6 @@ try:
                   help="output file path. End with .json or .html or .gz")
         @argument("--max_stack_depth", type=int, default=-1,
                   help="maximum stack depth you want to trace.")
-        @argument("--verbose", "-v", default=0, type=int,
-                  help="verbosity level")
         @argument("--ignore_c_function", action="store_true", default=False,
                   help="ignore all c functions including most builtin functions and libraries")
         @argument("--ignore_frozen", action="store_true", default=False,
@@ -41,7 +39,7 @@ try:
             file_path = options.output_file
             tracer_kwargs = {
                 "output_file": file_path,
-                "verbose": options.verbose,
+                "verbose": 0,
                 "max_stack_depth": options.max_stack_depth,
                 "ignore_c_function": options.ignore_c_function,
                 "ignore_frozen": options.ignore_frozen,
