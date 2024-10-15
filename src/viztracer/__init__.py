7822b451606cf801a9a6cef2aeecc2d35d8a84ff
Tian Gao <gaogaotiantian@hotmail.com>
2024-10-14 20:51:00 -0400

    Ready for 0.17.0

--- src/viztracer/__init__.py
+++ src/viztracer/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
 
-__version__ = "0.16.3"
+__version__ = "0.17.0"
 
 
 from .cellmagic import load_ipython_extension
