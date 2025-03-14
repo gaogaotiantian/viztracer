c5270629d0d4cdcc7fa68690d1405c6d4279fc0b
Tian Gao <gaogaotiantian@hotmail.com>
2025-03-14 15:43:07 -0400

    Limit coredumpy to github actions or explict enabling

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,12 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
 
-try:
-    import coredumpy
-    import os
-    coredumpy.patch_unittest(directory=os.getenv("COREDUMPY_DUMP_DIR", "./"))
-except ImportError:
-    pass
+import os
+
+
+if os.getenv("GITHUB_ACTIONS") or os.getenv("ENABLE_COREDUMPY"):
+    try:
+        import coredumpy
+        coredumpy.patch_unittest(directory=os.getenv("COREDUMPY_DUMP_DIR", "./"))
+    except ImportError:
+        pass
