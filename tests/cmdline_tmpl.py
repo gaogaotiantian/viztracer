5688ebace25fd27d715deef4b1d22e2481ee7a56
Tian Gao <gaogaotiantian@hotmail.com>
2025-12-25 13:58:52 -0800

    Okay pystack

--- tests/cmdline_tmpl.py
+++ tests/cmdline_tmpl.py
@@ -4,6 +4,7 @@
 import json
 import logging
 import os
+import psutil
 import shutil
 import signal
 import subprocess
@@ -66,6 +67,10 @@ class CmdlineTmpl(BaseTmpl):
             p.stderr.close()
             p.stdout, p.stderr = stdout, stderr
         except subprocess.TimeoutExpired:
+            for proc in psutil.Process(p.pid).children(recursive=True):
+                logging.error(f"Child process {proc.pid} info:")
+                proc_info = subprocess.check_output(["pystack", "remote", str(proc.pid)]).decode("utf-8")
+                logging.error(proc_info)
             if sys.platform == "win32":
                 p.terminate()
             else:
